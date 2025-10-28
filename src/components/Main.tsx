f957f845323b24bb5e494abc11dfac28d2b21d5a
bhavin8787 <bahvinc417@gmail.com>
2025-10-28 15:22:25 +0530

    first commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../assets/styles/Main.scss';
+
+function Main() {
+
+  return (
+    <div className="container">
+      <div className="about-section">
+        <div className="image-wrapper">
+          <img src="https://res.cloudinary.com/dtjh3dzal/image/upload/v1761636850/Untitled_design_9_j5y5bn.png" alt="Avatar" />
+        </div>
+        <div className="content">
+          <div className="social_icons">
+            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+          <h1>Bhavin Chaudhari</h1>
+          <p>Full Stack Engineer</p>
+
+          <div className="mobile_social_icons">
+            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
